ba49933ca7760b64fe4970cfbab32796cbcc7543
Pedro Escudero <pedro@MacBook-Pro-de-Pedro.local>
2018-09-22 22:52:33 +0200

    Change main to improve Rabbit call

--- go-service/main.go
+++ go-service/main.go
@@ -2,18 +2,11 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
-    "net/http"
     "time"
     "log"
     "github.com/streadway/amqp"
 )
 
-// Here's the worker, of which we'll run several
-// concurrent instances. These workers will receive
-// work on the `jobs` channel and send the corresponding
-// results on `results`. We'll sleep a second per job to
-// simulate an expensive task.
 func worker(id int, jobs <-chan int, results chan<- int) {
     for j := range jobs {
         fmt.Println("worker", id, "started  job", j)
@@ -32,39 +25,24 @@ func failOnError(err error, msg string) {
 
 func main() {
 
-    // In order to use our pool of workers we need to send
-    // them work and collect their results. We make 2
-    // channels for this.
     jobs := make(chan int, 100)
     results := make(chan int, 100)
 
-    // This starts up 3 workers, initially blocked
-    // because there are no jobs yet.
     for i := 1; i<10; {
       for w := 1; w <= 3; w++ {
           go worker(w, jobs, results)
       }
 
-      // Here we send 5 `jobs` and then `close` that
-      // channel to indicate that's all the work we have.
       for j := 1; j <= 5; j++ {
           jobs <- j
       }
       //close(jobs)
 
-
-      //
-      resp, err := http.Get("https://google.com")
-      body, err := ioutil.ReadAll(resp.Body)
-      fmt.Println(err)
-      fmt.Println(body)
-      //
-      // Finally we collect all the results of the work.
       for a := 1; a <= 5; a++ {
           <-results
       }
 
-      conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+      conn, err := amqp.Dial("amqp://guest:guest@192.20.128.2:5672")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
